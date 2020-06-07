8b1defac8bee54f8d648e136fa7c280fb7ab53c0
Dave <dave.gagnat@gmail.com>
2020-06-06 20:06:30 -0500

    Updated navbar to include social links for all sites

--- frontend/components/nav/nav.jsx
+++ frontend/components/nav/nav.jsx
@@ -25,17 +25,18 @@ class Nav extends React.Component {
           </div>
 
           <div className="nav-icons nav-three">
-            <div className="nav-icon-links">
-              <i className="far fa-compass"></i>
-            </div>
-            <div className="nav-icon-links">
-              <i className="far fa-heart"></i>
-            </div>
-            <Link to={`/users/${userId}`} className="nav_user_icon_link">
-              <div className="nav-icon-links">
-                <i className="far fa-user"></i>
-              </div>
-            </Link>
+            <ul className='nav-social-links'>
+              <li><a href="https://www.linkedin.com/in/davegagnat/"><i className="fab fa-linkedin linkedin-color"></i></a></li>
+              <li><a href="https://github.com/12Starlight"><i className="fab fa-github github-color"></i></a></li>
+              <li><a href="https://twitter.com/hashtag/TheCodeEffect?src=hash"><i className="fab fa-twitter twitter-color"></i></a></li>
+              <li><a href="https://angel.co/u/dave-gagnat"><i className="fab fa-angellist angellist-color"></i></a></li>
+              <Link to={`/users/${userId}`} className="">
+                <div className="">
+                  <i className="far fa-user user-profile"></i>
+                </div>
+              </Link>
+              <li><a href='https://www.davegagnat.com'><i className="fas fa-globe-americas world"></i></a></li>
+            </ul>
           </div>
         </div>
       </nav>
