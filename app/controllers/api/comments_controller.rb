afe1fc9f96b4c83709ecc008f194d9223032f434
Dave <dave.gagnat@gmail.com>
2020-12-24 18:10:28 -0600

    Updated SessionController

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -10,7 +10,7 @@ class Api::CommentsController < ApplicationController
     else
       @comments = Comment.all 
     end
-    # render json: comments
+    # render json: commentsr
     render :index 
   end 
 
